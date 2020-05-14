6421f13de87c4c813c6b70b8ddf5d163b1179ba0
guhao02 <guhao02@qutoutiao.net>
2020-05-15 00:20:59 +0800

    装饰器模式

--- decorator/decorator2.go
+++ decorator/decorator2.go
@@ -8,17 +8,19 @@ import (
 //区别于decorator.go文件中的实现方式，此处采用函数式编程的方式实现装饰器模式。
 
 //装饰器1：加密装饰器
-func Crypt(f func()) func(string) {
-	return func(salt string) {
+func Crypt(f func()) func(string) error {
+	return func(salt string) error {
 		f()
 		fmt.Printf("crypt with %s...\n", salt)
+		return nil
 	}
 }
 
 //装饰器2：缓冲装饰器
-func Buffer(f func()) func(reflect.Type) {
-	return func(p reflect.Type) {
+func Buffer(f func()) func(reflect.Type) error {
+	return func(p reflect.Type) error {
 		f()
 		fmt.Println("buffered:", p)
+		return nil
 	}
 }
